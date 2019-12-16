a7dbe91ff7bd0f39d939e00705978e9b4056e7d7
jean.miranda - JMN <jean.miranda@iteris.com.br>
2019-12-16 09:23:06 -0300

    Population funcionando porém erro no uriom

--- 1160/1160.js
+++ 1160/1160.js
@@ -1,25 +1,24 @@
 const input = require("fs").readFileSync("./1160.txt", "utf8");
 const lines = input.split("\r\n");
 
-let p1, p2;
-let g1, g2;
-
-for (let [index, line] of lines.entries()) {
+lines.forEach((line, index) => {
   if (index > 0) {
+    let p1, p2;
+    let g1, g2;
     let items = line.split(" ");
     let anos = 0;
     let sec = 0;
 
     //   items
-    p1 = items[0];
-    p2 = items[1];
-    g1 = items[2];
-    g2 = items[3];
+    p1 = parseFloat(items[0]);
+    p2 = parseFloat(items[1]);
+    g1 = parseFloat(items[2]);
+    g2 = parseFloat(items[3]);
 
-    console.log(p1)
     while (p1 <= p2) {
-      p1 += (p1 * g1) / 100;
-      p2 += (p2 * g2) / 100;
+      p1 += Math.round((p1 * g1) / 100);
+      p2 += Math.round((p2 * g2) / 100);
+
       anos++;
 
       if (anos > 100) {
@@ -28,6 +27,10 @@ for (let [index, line] of lines.entries()) {
       }
     }
 
-    // console.log(anos);
+    if (sec === 0) {
+      console.log(anos + " anos.");
+    } else {
+      console.log("Mais de 1 seculo.");
+    }
   }
-}
+});
